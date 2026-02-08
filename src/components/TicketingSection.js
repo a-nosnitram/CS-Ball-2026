ce2060662c7a30dd85d54e0cf8d4992e52231f3a
a-nosnitram <stasymartinson@gmail.com>
2026-02-08 18:54:07 +0000

    chore(tickets): update info

--- src/components/TicketingSection.js
+++ src/components/TicketingSection.js
@@ -8,26 +8,32 @@ function TicketingSection() {
         <div className="ticket-info">
           <div className="ticket-tier">
             <h3>Regular</h3>
-            <p className="price">£45</p>
-            <p className="details">Limited fhjgsudfgn</p>
-            <p className="details">Available on dfhfhd</p>
+            <p className="price">£40.5</p>
+            <p className="details">
+              Limited Availability, <br />
+              get it while you can!
+            </p>
+            <p className="details">Drop on Feb 20, 12pm</p>
+          </div>
+          <div className="ticket-tier">
+            <h3>Dinner</h3>
+            <p className="price">£45.5</p>
+            <p className="details">
+              Includes a three-course meal,
+              <br /> wine, and the after-party!
+            </p>
+            <p className="details">Drop on Feb 27, 12pm</p>
           </div>
           <div className="ticket-tier">
             <h3>Afterparty</h3>
-            <p className="price">£30</p>
-            <p className="details">General admission</p>
-            <p className="details">Available from DATE</p>
-          </div>
-          <div className="ticket-tier">
-            <h3>After-Afterparty</h3>
-            <p className="price">£20</p>
-            <p className="details">hdfhdfh</p>
-            <p className="details">Only access to afterparty</p>
-            <p className="details">Complimentary drinks</p>
+            <p className="price">£20.5</p>
+            <p className="details">
+              Includes a ceilidh, poker, a DJ, <br /> and photography all night!
+            </p>
+            <p className="details">Drop date unknown</p>
           </div>
         </div>
         <div className="ticket-note">
-          {/* <p>All tickets include frhfrhng</p> */}
           <p>Dress code: Formal attire with masks encouraged</p>
         </div>
       </div>
