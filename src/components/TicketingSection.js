70b9f52208a3946e868df9d0ae14da467ce5f10b
a-nosnitram <stasymartinson@gmail.com>
2026-02-11 18:52:35 +0000

    chore(tickets): update mobile leayout (columns)

--- src/components/TicketingSection.js
+++ src/components/TicketingSection.js
@@ -7,30 +7,42 @@ function TicketingSection() {
         <h2 className="ticketing-title">Ticketing Information</h2>
         <div className="ticket-info">
           <div className="ticket-tier">
-            <h3>Regular</h3>
-            <p className="price">£40.5</p>
-            <p className="details">
-              Limited Availability, <br />
-              get it while you can!
-            </p>
-            <p className="details">Drop on Feb 20, 12pm</p>
+            <div className="ticket-tier-col1">
+              <h3>Regular</h3>
+              <p className="price">£40.5</p>
+            </div>
+            <div className="ticket-tier-col2">
+              <p className="details">
+                Limited Availability, <br />
+                get it while you can!
+              </p>
+              <p className="details">Drop on Feb 20, 12pm</p>
+            </div>
           </div>
           <div className="ticket-tier">
-            <h3>Dinner</h3>
-            <p className="price">£45.5</p>
-            <p className="details">
-              Includes a three-course meal,
-              <br /> wine, and the after-party!
-            </p>
-            <p className="details">Drop on Feb 27, 12pm</p>
+            <div className="ticket-tier-col1">
+              <h3>Dinner</h3>
+              <p className="price">£45.5</p>
+            </div>
+            <div className="ticket-tier-col2">
+              <p className="details">
+                Includes: three-course meal,
+                <br /> wine, and the after-party!
+              </p>
+              <p className="details">Drop on Feb 27, 12pm</p>
+            </div>
           </div>
           <div className="ticket-tier">
-            <h3>Afterparty</h3>
-            <p className="price">£20.5</p>
-            <p className="details">
-              Includes a ceilidh, poker, a DJ, <br /> and photography all night!
-            </p>
-            <p className="details">Drop date unknown</p>
+            <div className="ticket-tier-col1">
+              <h3>Afterparty</h3>
+              <p className="price">£20.5</p>
+            </div>
+            <div className="ticket-tier-col2">
+              <p className="details">
+                Includes: ceilidh, poker, DJ, <br /> and photography all night!
+              </p>
+              <p className="details">Drop date unknown</p>
+            </div>
           </div>
         </div>
         <div className="ticket-note">
